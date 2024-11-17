51bdfd6506acafa0721906608ce7cd1ad420eead
slannec <nick104@outlook.com>
2024-11-17 15:03:05 -0300

    testing workflow

--- app.py
+++ app.py
@@ -22,3 +22,5 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     app.run(host=app.config["HOST"], port=app.config['PORT'], debug=app.config['DEBUG'])
+
+#Adicionando linha para testar o workflow
